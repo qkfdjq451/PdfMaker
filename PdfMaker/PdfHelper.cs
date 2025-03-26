32c05390439d08edb11aebd90eff01dd7b5452b5
이동인 <>
2025-03-26 15:10:25 +0900

    빌드 스크립트로 tessdata 자동으로 받게 설정

---
+++ PdfMaker/PdfHelper.cs
@@ -0,0 +1,11 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using Tesseract;
+
+public class PdfHelper
+{
+    public static void ToPdf(string pngPath)
+    {
+
+    }
+}
---
+++ PdfMaker/Scripts/PreBuildScripts/Update Tessdata.ps1
@@ -0,0 +1,29 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$ProjectDir
+)
+
+$tessDataFiles = @('eng.traineddata', 'kor_vert.traineddata')
+$repoUrl = 'https://github.com/tesseract-ocr/tessdata/raw/main/'
+$tessDataDir = "$ProjectDir\tessdata"
+
+if (-not(Test-Path $tessDataDir)) {
+    New-Item -ItemType Directory -Path $tessDataDir
+}
+
+foreach ($file in $tessDataFiles) {
+    $url = $repoUrl + $file
+    $target = Join-Path $tessDataDir $file
+
+    Write-Host "Checking $file for updates..."
+
+    $webRequest = Invoke-WebRequest $url -Method Head
+    $remoteSize = $webRequest.Headers.'Content-Length'
+
+    if (-not(Test-Path $target) -or ((Get-Item $target).Length -ne $remoteSize)) {
+        Write-Host "Downloading latest $file..."
+        Invoke-WebRequest -Uri $url -OutFile $target
+    } else {
+        Write-Host "$file is already up to date."
+    }
+}
